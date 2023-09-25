2b456f0182bdd662066b94c0b51526b77cb88444
Thiago Ribeiro <thiagoribeirods@outlook.com>
2023-09-25 12:07:04 -0300

    remocao do cascade

--- src/main/java/br/gov/sp/franciscomorato/educacao/processoseletivo/model/User.java
+++ src/main/java/br/gov/sp/franciscomorato/educacao/processoseletivo/model/User.java
@@ -3,6 +3,7 @@ package br.gov.sp.franciscomorato.educacao.processoseletivo.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
@@ -14,6 +15,7 @@ import java.util.Collection;
 import java.util.List;
 
 @Entity
+@Data
 public class User implements UserDetails
 {
     @Id
