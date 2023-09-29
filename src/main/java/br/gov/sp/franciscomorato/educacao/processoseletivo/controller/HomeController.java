8476cb3f1a885d3617f0f9cdaa22f1008e170833
Thiago Ribeiro <thiagoribeirods@outlook.com>
2023-09-29 10:05:13 -0300

    parametro username enviado para view

--- src/main/java/br/gov/sp/franciscomorato/educacao/processoseletivo/controller/HomeController.java
+++ src/main/java/br/gov/sp/franciscomorato/educacao/processoseletivo/controller/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.gov.sp.franciscomorato.educacao.processoseletivo.model.Role;
 import br.gov.sp.franciscomorato.educacao.processoseletivo.model.User;
+import br.gov.sp.franciscomorato.educacao.processoseletivo.service.CandidateService;
 import br.gov.sp.franciscomorato.educacao.processoseletivo.service.SelectiveProcessService;
 import br.gov.sp.franciscomorato.educacao.processoseletivo.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -25,17 +26,22 @@ public class HomeController
 
     @Autowired
     private SelectiveProcessService processService;
+
+    @Autowired
+    private CandidateService candidateService;
     
     @GetMapping
     public String home(Authentication authentication, Model model)
     {
         if( authentication.getAuthorities().contains(new Role("ROLE_ADMIN")) ||
-            authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ROOT"))
+        authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ROOT"))
         ) {
+            model.addAttribute("username", authentication.getName());
             return "redirect:/process";
         }
 
         model.addAttribute("processList", processService.findInProgress());
+        model.addAttribute("username", candidateService.findByCpf(Long.parseLong(authentication.getName())));
         
         return "candidate/home";
     }
