b2f175c24eda4d96ab65b2c32360346b23a48993
Thiago Ribeiro <thiagoribeirods@outlook.com>
2023-09-29 10:41:18 -0300

    view de inscrição do processo seletivo

---
+++ src/main/java/br/gov/sp/franciscomorato/educacao/processoseletivo/controller/SubscriptionController.java
@@ -0,0 +1,37 @@
+package br.gov.sp.franciscomorato.educacao.processoseletivo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.gov.sp.franciscomorato.educacao.processoseletivo.model.SelectiveProcess;
+import br.gov.sp.franciscomorato.educacao.processoseletivo.model.Subscription;
+import br.gov.sp.franciscomorato.educacao.processoseletivo.service.SelectiveProcessService;
+
+
+@Controller
+@RequestMapping("/subscription")
+public class SubscriptionController 
+{
+    
+    @Autowired
+    private SelectiveProcessService processService;
+
+    @GetMapping("/{processId}")
+    public String subscriptionView(@PathVariable Integer processId, Model model)
+    {
+
+        SelectiveProcess process = processService.findById(processId);
+
+        if(process != null)
+            model.addAttribute("subscription", new Subscription(process));
+        else
+            return "error/404";
+
+        return "subscription/subscription";
+    }
+    
+}
--- src/main/java/br/gov/sp/franciscomorato/educacao/processoseletivo/model/Subscription.java
+++ src/main/java/br/gov/sp/franciscomorato/educacao/processoseletivo/model/Subscription.java
@@ -1,9 +1,17 @@
 package br.gov.sp.franciscomorato.educacao.processoseletivo.model;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.Data;
 
 @Entity
@@ -13,6 +21,34 @@ public class Subscription
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createAt;
+
+    @ManyToOne
+    private SelectiveProcess process;
+
+    @ManyToOne
+    private Candidate candidate;
+
+    @ManyToMany
+    private List<Modality> modalities = new ArrayList<>();
+
+    public Subscription () {}
+
+    public Subscription (SelectiveProcess selectiveProcess) {
+        this.process = selectiveProcess;
+    }
+
+    public boolean addModality(Modality modality) 
+    {
+        return this.modalities.add(modality);
+    }
+
+    public boolean addModalities(List<Modality> modalities)
+    {
+        return this.modalities.addAll(modalities);
+    }
 
     
     
