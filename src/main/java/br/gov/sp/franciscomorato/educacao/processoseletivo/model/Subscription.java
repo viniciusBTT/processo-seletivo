8db98c843fd4ac0a8ea1e665cf2c6b9f130a563d
Thiago Ribeiro <thiagoribeirods@outlook.com>
2023-10-04 15:30:47 -0300

    fetch type eager

--- src/main/java/br/gov/sp/franciscomorato/educacao/processoseletivo/model/Subscription.java
+++ src/main/java/br/gov/sp/franciscomorato/educacao/processoseletivo/model/Subscription.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.List;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -31,7 +32,7 @@ public class Subscription
     @ManyToOne
     private Candidate candidate;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     private List<Modality> modalities = new ArrayList<>();
 
     public Subscription () {}
