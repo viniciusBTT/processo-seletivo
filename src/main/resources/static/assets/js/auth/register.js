190d296572ddea770ed28c478e71fe3aa84dd614
viniciusBTT <vini.barrost@gmail.com>
2023-10-05 16:57:14 -0300

    vericações nos inputs de register e correções de interface

--- src/main/resources/static/assets/js/auth/register.js
+++ src/main/resources/static/assets/js/auth/register.js
@@ -15,13 +15,83 @@ function showSpecialCondition(){
 
 
 
-//Verificando se as senhas são iguais
+//Captura o envento de submit para fazer as devidas verificações
 let registerForm = document.querySelector("#formRegister")
 registerForm.addEventListener("submit", e => {
     e.preventDefault();
-    console.log(e)
-    if(document.querySelector("#password").value === document.querySelector("#confirmPassword").value)
-        e.currentTarget.submit();    
+
+    document.querySelector('#cpf').value = document.querySelector('#cpf').value.replace(/[^a-zA-Z0-9 ]/g,'')   
+    //se a senha não confere retorna msg de rro
+    if(document.querySelector("#password").value !== document.querySelector("#confirmPassword").value)
+    {
+        Swal.fire({
+            icon: 'error',
+            html: 'As senhas digitadas nao conferem',
+            timerProgressBar: true,     
+            background: '#f1f1f1 ',                  
+            backdrop: "rgba(0, 0, 0, 0)" ,
+            })
+    }    
     else
-        alert("À senhas estão diferentes ")
+    {
+        e.currentTarget.submit();   
+    }
 })
+
+
+function verifyCPF(){
+    let cpfValide = document.querySelector('#cpf').value.replace(/[^a-zA-Z0-9 ]/g,'')
+    
+    if(testaCPF(cpfValide))
+    {        //verificando se o cpf está valido e se já foi cadastrado
+        axios.get(`/register/candidate?cpf=${cpfValide}`)
+        .then(function (response) {
+            console.log(response);
+            cpfValide = response.data;
+
+            Swal.fire({
+                icon: 'info',
+                html: cpfValide,
+                timerProgressBar: true,     
+                background: '#f1f1f1 ',                  
+                backdrop: "rgba(0, 0, 0, 0)" ,
+                })
+        })
+        .catch(function (error) {
+            console.error(`error: ${error}`);
+        })
+    }
+    else
+    {
+        Swal.fire({
+            icon: 'error',
+            html: 'valor do cpf invalido',
+            timerProgressBar: true,     
+            background: '#f1f1f1 ',                  
+            backdrop: "rgba(0, 0, 0, 0)" ,
+            })
+    }
+
+    
+}
+
+function testaCPF(strCPF) {
+    var Soma;
+    var Resto;
+    Soma = 0;
+  if (strCPF == "00000000000") return false;
+
+  for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+  Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
+
+  Soma = 0;
+    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
+    return true;
+}
