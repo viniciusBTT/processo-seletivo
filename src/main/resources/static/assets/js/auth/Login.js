12e88314ea38e4234db858be8190aed7c15e6830
viniciusBTT <vini.barrost@gmail.com>
2023-10-16 17:00:03 -0300

    correções no login

--- src/main/resources/static/assets/js/auth/Login.js
+++ src/main/resources/static/assets/js/auth/Login.js
@@ -28,3 +28,59 @@ let username = document.getElementById("username")
                     console.log(document.getElementById("username").value)                
                     e.currentTarget.submit();
             })
+
+function forgetPassword()
+{
+    Swal.fire({
+        title: 'Informe seu CPF para prosseguir com a troca de senha:',
+        input: 'text',
+        inputAttributes: {
+          autocapitalize: 'off'
+        },
+        showCancelButton: true,
+        cancelButtonText: 'calcelar',
+        confirmButtonText: 'Prosseguir',
+        showLoaderOnConfirm: true,
+        preConfirm: (cpf) => {
+            console.log(cpf)
+          return fetch(`//api.github.com/users/${cpf}`)
+            .then(response => {
+              if (!response.ok) {
+                throw new Error(response.statusText)
+              }
+              return response.json()
+            })
+            .catch(error => {
+              Swal.showValidationMessage(
+                `Por favor, verifique o seu CPF`
+              )
+            })
+        },
+        allowOutsideClick: () => !Swal.isLoading()
+      }).then((result) => {
+        console.log(result)
+        if (result.isConfirmed) 
+            confirmEmail()        
+      })
+}
+
+function confirmEmail(username,email)
+{
+    Swal.fire({
+        title: `Candidato "46069293835"`,
+        text: 'Èste é o seu email: ',
+        showDenyButton: true,
+        showCancelButton: false,
+        confirmButtonText: 'Sim',
+        denyButtonText: `Não`,
+      }).then((result) => {
+        /* verificando a responsa da modal*/
+        if (result.isConfirmed) 
+        {
+            Swal.fire('Encaminhamos ao seu e-mail o link para realizar a alteração da senha', '', 'success')
+        }
+        else if (result.isDenied) 
+          Swal.fire('Tente alterar a senha novamente', '', 'info')
+        
+      })
+}
