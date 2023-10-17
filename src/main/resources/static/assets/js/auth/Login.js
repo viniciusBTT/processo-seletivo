214f50b42ee6bf4722da022c0b23d2f5cd607fa6
viniciusBTT <vini.barrost@gmail.com>
2023-10-17 12:34:10 -0300

    front end da mudança de senha

--- src/main/resources/static/assets/js/auth/Login.js
+++ src/main/resources/static/assets/js/auth/Login.js
@@ -1,86 +1,93 @@
-let username = document.getElementById("username")  
-            let lblLogin = document.getElementById('lblUsername');
-            username.addEventListener('input', e =>{
+let username = document.getElementById("username")
+let lblLogin = document.getElementById('lblUsername');
+username.addEventListener('input', e => {
 
-                if(e.target.value.length === 0)
-                {
-                    VMasker(username).unMask();
-                    lblLogin.innerText = '';
-                    lblLogin.innerText = 'CPF ou Nome de Usuário';
-                    return;
-                }
+    if (e.target.value.length === 0) {
+        VMasker(username).unMask();
+        lblLogin.innerText = '';
+        lblLogin.innerText = 'CPF ou Nome de Usuário';
+        return;
+    }
 
-                if (!isNaN(e.target.value)) {
-                    //alert('é char');
-                    VMasker(username).maskPattern("999.999.999-99"); 
-                    lblLogin.innerText = 'CPF';
-                }
-                else
-                {
-                    lblLogin.innerText = 'Nome de Usuário';                   
-                }
-            })
-            document.getElementById('formLogin').addEventListener('submit', e => {
-                e.preventDefault(); 
-                console.log(document.getElementById("username").value)                
-                if(!isNaN(username.value[0]))
-                    document.getElementById("username").value = document.getElementById("username").value.replaceAll(".", "").replaceAll("-", "");
-                    console.log(document.getElementById("username").value)                
-                    e.currentTarget.submit();
-            })
+    if (!isNaN(e.target.value)) {
+        VMasker(username).maskPattern("999.999.999-99");
+        lblLogin.innerText = 'CPF';
+    }
+    else {
+        lblLogin.innerText = 'Nome de Usuário';
+    }
+})
+document.getElementById('formLogin').addEventListener('submit', e => {
+    e.preventDefault();
+    console.log(document.getElementById("username").value)
+    if (!isNaN(username.value[0]))
+        document.getElementById("username").value = document.getElementById("username").value.replaceAll(".", "").replaceAll("-", "");
+    console.log(document.getElementById("username").value)
+    e.currentTarget.submit();
+})
 
-function forgetPassword()
-{
+function forgetPassword() {
     Swal.fire({
         title: 'Informe seu CPF para prosseguir com a troca de senha:',
         input: 'text',
         inputAttributes: {
-          autocapitalize: 'off'
+            autocapitalize: 'off'
         },
         showCancelButton: true,
-        cancelButtonText: 'calcelar',
+        cancelButtonText: 'Calcelar',
         confirmButtonText: 'Prosseguir',
         showLoaderOnConfirm: true,
         preConfirm: (cpf) => {
             console.log(cpf)
-          return fetch(`//api.github.com/users/${cpf}`)
-            .then(response => {
-              if (!response.ok) {
-                throw new Error(response.statusText)
-              }
-              return response.json()
+            return axios.get('/forgot/find', {
+                params: {
+                    cpf: cpf.replace(/[^a-zA-Z0-9 ]/g,''),
+                }
             })
-            .catch(error => {
-              Swal.showValidationMessage(
-                `Por favor, verifique o seu CPF`
-              )
-            })
+            .then((response)=> {
+                return response.data;
+                
+            }) .catch(function (error) {
+                return null
+              })
         },
         allowOutsideClick: () => !Swal.isLoading()
-      }).then((result) => {
-        console.log(result)
-        if (result.isConfirmed) 
-            confirmEmail()        
-      })
+    }).then((result) => {
+        if (result.value)
+            confirmEmail(result.value)
+        else
+            Swal.fire('CPF invalido', '', 'info')
+    })
 }
 
-function confirmEmail(username,email)
-{
+
+function confirmEmail(candidate) {
     Swal.fire({
-        title: `Candidato "46069293835"`,
-        text: 'Èste é o seu email: ',
+        title: `Candidato ${candidate.candidate}`,
+        text: `Email : ${candidate.email}`,
         showDenyButton: true,
         showCancelButton: false,
         confirmButtonText: 'Sim',
         denyButtonText: `Não`,
-      }).then((result) => {
+    }).then((result) => {
         /* verificando a responsa da modal*/
-        if (result.isConfirmed) 
-        {
+        if (result.isConfirmed) {
             Swal.fire('Encaminhamos ao seu e-mail o link para realizar a alteração da senha', '', 'success')
+            axios.post('/forgot/reset', {
+                ForgotDTO: {
+                    candidate: candidate.candidate,
+                    email: candidate.email
+                }
+              })
+              .then(function (response) {
+                console.log(response);
+              })
+              .catch(function (error) {
+                console.error(error);
+              });
         }
-        else if (result.isDenied) 
-          Swal.fire('Tente alterar a senha novamente', '', 'info')
-        
-      })
+        else if (result.isDenied)
+            Swal.fire('Tente alterar a senha novamente', '', 'info')
+
+    })
 }
